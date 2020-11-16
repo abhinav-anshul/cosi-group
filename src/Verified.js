3dec6e8ede3bf97db97522e22d705e32656cc824
abhinav anshul <abhinavanshul4u@gmail.com>
2020-11-16 10:56:11 +0530

    adds error page

--- src/Verified.js
+++ src/Verified.js
@@ -1,11 +1,25 @@
 import React from "react"
+import { motion } from "framer-motion"
+import { CheckCircleIcon } from "@chakra-ui/icons"
+import { Text, Button } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 
 const Verified = () => {
   return (
     <>
-      <div>
-        <p>hi</p>
-        <hi>i</hi>
+      <div style={{ marginTop: "150px", textAlign: "center" }}>
+        <Text fontSize='4xl'>You're all set 💪 </Text>
+        <CheckCircleIcon w={12} h={12} color='teal.500' />
+        <br />
+        <br />
+        <Link to='/welcome'>
+          <Text>
+            Goto{" "}
+            <Button colorScheme='teal' size='xs'>
+              Home
+            </Button>
+          </Text>
+        </Link>
       </div>
     </>
   )
